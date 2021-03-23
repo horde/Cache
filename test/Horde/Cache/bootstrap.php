7cab8f3d74c0a21ebc79fe0c7783870111b6b5d9
abbrancati <abbrancati@b1-systems.de>
2021-03-23 09:51:12 +0000

    no errors, added ci.yml

--- test/Horde/Cache/bootstrap.php
+++ test/Horde/Cache/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 if (!class_exists('Horde_Test_Bootstrap')) {
-    		require_once 'Horde/Test/Bootstrap.php';
+    require_once 'Horde/Test/Bootstrap.php';
 }
 Horde_Test_Bootstrap::bootstrap(dirname(__FILE__));
