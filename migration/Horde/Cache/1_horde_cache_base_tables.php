6712b466a14769ba0e6bcf22c8c28075ac95154f
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-01 17:58:11 +0200

    chore(metadata): Expose we now provide a PSR-16 implementation and require a PSR-3 logger

--- migration/Horde/Cache/1_horde_cache_base_tables.php
+++ migration/Horde/Cache/1_horde_cache_base_tables.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @category Horde
  * @internal
@@ -9,10 +10,10 @@ class HordeCacheBaseTables extends Horde_Db_Migration_Base
     public function up()
     {
         if (!in_array('horde_cache', $this->tables())) {
-            $t = $this->createTable('horde_cache', array('autoincrementKey' => array('cache_id')));
-            $t->column('cache_id', 'string', array('limit' => 32, 'null' => false));
-            $t->column('cache_timestamp', 'bigint', array('null' => false));
-            $t->column('cache_expiration', 'bigint', array('null' => false));
+            $t = $this->createTable('horde_cache', ['autoincrementKey' => ['cache_id']]);
+            $t->column('cache_id', 'string', ['limit' => 32, 'null' => false]);
+            $t->column('cache_timestamp', 'bigint', ['null' => false]);
+            $t->column('cache_expiration', 'bigint', ['null' => false]);
             $t->column('cache_data', 'binary');
             $t->end();
         }
--- migration/Horde/Cache/2_horde_cache_fix_blob_length.php
+++ migration/Horde/Cache/2_horde_cache_fix_blob_length.php
@@ -1,4 +1,5 @@
 <?php
+
 class HordeCacheFixBlobLength extends Horde_Db_Migration_Base
 {
     public function up()
@@ -6,7 +7,5 @@ class HordeCacheFixBlobLength extends Horde_Db_Migration_Base
         $this->changeColumn('horde_cache', 'cache_data', 'binary');
     }
 
-    public function down()
-    {
-    }
+    public function down() {}
 }
